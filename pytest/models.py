d63468a5afa857a8dd134a96bfc536d97d2f299c
Sebastian Agredo <sebastian.agredo@uniminuto.edu.co>
2024-11-23 02:30:50 +0000

    Practica TEST

---
+++ pytest/models.py
@@ -0,0 +1,98 @@
+class Person:
+    def __init__(self,name,lastName,age,dni):
+        self.name = name 
+        self.lastName = lastName
+        self.age = age
+        self.dni = dni
+
+
+"""
+CRUD
+
+CREATE
+READ
+UPDATE
+DELETE
+"""
+
+"""
+This method provide information about the addition between 
+
+
+"""
+class PersonCRUD:
+    def __init__(self):
+        self.people=[]
+
+# """ 
+# Es una función de busqueda por DNI
+
+# Params:
+# DNI: Es un atributo de la clase Person (string | number)
+
+# return:
+# Un objeto de tipo person
+
+# """
+
+    def find_by_dni(self,dni):
+        for person in self.persons:
+            if person.dni == dni:
+                return person
+        
+        return None
+
+    def create(self,person):
+        if self.find_by_dni(person.dni):
+            raise ValueError("Person already exists")
+        self.persons.append(person)
+
+        #Optional
+        return 
+
+    def read(self,dni):
+        person = self.find_by_dni(dni)
+        if not person:
+            raise ValueError("Person not fount")
+
+        return person
+
+    def update(self,dni,name,lastName,age):
+        person = self.find_by_dni(dni)
+        if not person:
+            raise ValueError("Person not fount")
+        
+        if name is not None:
+            person.name = name 
+        
+        if lastName is not None:
+            person.lastName = lastName 
+
+        if age is not None:
+            person.age = age 
+
+        if name is not None:
+            person.name = name 
+
+        #Optional
+        return person
+    
+    # def _update (self,dni,**kwargs):
+    #     person = self.find_by_dni(dni)
+
+    #      if not person:
+    #         raise ValueError("Person not fount")
+
+    #     for key, value in kwargs.items():
+    #         if hasattr(person, key):
+    #             setattr(person, key, value)
+    
+    #     return person
+
+    def delete(self,dni):
+        person = self.find_by_dni(dni)
+        if not person:
+            raise ValueError("Person not fount")
+        self.persons.remove(person)
+
+        return f("Person deleted")
---
+++ pytest/test_models.py
@@ -0,0 +1,24 @@
+import unittest
+from models import PersonCRUD
+from models import Person
+
+class TestPersonCRUDE(unittest.TestCase):
+
+    def setUp(self):
+        self.crud = PersonCRUD()
+        self.person1 = Person("Sebastián","Agredo","23","1006073732")
+        self.person2 = Person("Fredy","Agredo","79","14445065")
+
+    def test_create(self):
+        self.crud.create(self.person1)
+
+        self.assertEqual(len(self.crud.persons), 1)
+        self.assertEqual(self.crud.persons[0].name, "Sebastián")
+
+    def test_person_duplicate_dni(self):
+        self.crud.create(self.person1)
+        with self.assertRaises(ValueError):
+            self.crud.create(self.person)
+
+if __name__ == "__main__":
+    unittest.main()
